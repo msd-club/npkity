c5b93287817bd14115cd12962c4ad85a38ad6adb
Ralph Maron A. Eda <128874876+ralphmarondev@users.noreply.github.com>
2025-12-05 16:05:00 +0800

    revert changes

--- models/plots.ts
+++
@@ -1,9 +0,0 @@
-export type Plots = {
-  id: number
-  location: string
-  description: string
-  created_at: Date
-  updated_at: Date
-  deleted_at: Date
-  is_deleted: boolean
-}
--- models/user.ts
+++
@@ -1,8 +0,0 @@
-export type User = {
-  id: string
-	email: string
-	username: string 
-	password: string 
-	created_at: Date 
-	updated_at: Date 
-}
